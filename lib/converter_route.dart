c1f9f36a3d2909fd660cc242293c1b5946dde996
Nicolas <nicolas.rod.f@gmail.com>
2018-08-04 17:54:31 -0500

    Initial commit

---
+++ lib/converter_route.dart
@@ -0,0 +1,69 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+import 'package:solution_05_stateful_widgets/unit.dart';
+
+/// Converter screen where users can input amounts to convert.
+///
+/// Currently, it just displays a list of mock units.
+///
+/// While it is named ConverterRoute, a more apt name would be ConverterScreen,
+/// because it is responsible for the UI at the route's destination.
+/// //Se requiere que sea Stateful ya q se va este pantalla es interactiva. (checkbox, input, tap, etc.)
+class ConverterRoute extends StatefulWidget {
+  /// This [Category]'s name.
+  final String name;
+
+  /// Color for this [Category].
+  final Color color;
+
+  /// Units for this [Category].
+  final List<Unit> units;
+
+  /// This [ConverterRoute] requires the name, color, and units to not be null.
+  const ConverterRoute({
+    @required this.name,
+    @required this.color,
+    @required this.units,
+  })  : assert(name != null),
+        assert(color != null),
+        assert(units != null);
+
+  //Stateful widget tiene un estado, asociada a una nueva clase que extiende la clase State y q contiene nuestro Widget en este caso de "units"
+  @override
+  _ConverterRouteState createState() => _ConverterRouteState();
+}
+
+class _ConverterRouteState extends State<ConverterRoute> {
+  @override
+  Widget build(BuildContext context) {
+    // Here is just a placeholder for a list of mock units
+    final unitWidgets = widget.units.map((Unit unit) { //se agrega la palabra widget.
+      return Container(
+        color: widget.color, //aqui igual
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              unit.name,
+              style: Theme.of(context).textTheme.headline,
+            ),
+            Text(
+              'Conversion: ${unit.conversion}',
+              style: Theme.of(context).textTheme.subhead,
+            ),
+          ],
+        ),
+      );
+    }).toList();
+
+    return ListView(
+      children: unitWidgets,
+    );
+  }
+}
